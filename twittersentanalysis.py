f1748e2002af8be682a226ab1ac7ba2e17851351
shimonaprabhu <0718shimona@gmail.com>
2021-11-17 21:32:42 -0800

    Update twittersentanalysis.py

--- twittersentanalysis.py
+++ twittersentanalysis.py
@@ -2,12 +2,6 @@ import tweepy
 import sys
 from textblob import TextBlob
 
-consumer_key='YOlFFPSJJKbKzgEbuMhuU2JRN'
-consumer_secret='AYYgR0yg5poxO3n6eB0DfSObX90EVxOnbZ5BGPMORrQNdEITBf'
-
-access_token='861214691938271233-UES8Neymh63zsvLWNmclJqgPVcssGRi'
-access_token_secret='fsUdhNJq4M39uRShig6R8avauXq9ZRAyfZkPfXf9Nl10F'
-
 auth=tweepy.OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_token,access_token_secret)
 
